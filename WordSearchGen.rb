6b1cddedce6a4d77b211e5cd78cc5941145ae965
cvalleroy <cvalleroy@hmc.edu>
2018-02-06 20:30:17 -0800

    Added comments and corrected literal edge cases

--- WordSearchGen.rb
+++ WordSearchGen.rb
@@ -1,32 +1,36 @@
 class WordSearchGen
 
   inputWords = ["apple", "banana", "pear", "orange"]
-
-  wordSearchArray = Array.new(20) {Array.new(20, "/")}
-
-  wordSearchArray.each_index do |repl|
-    wordSearchArray[repl].each_index do |lett|
-	  wordSearchArray[repl][lett] = (65 + rand(26)).chr
+  /create the blank 20x20 array/
+  wordSearchArray = Array.new(20) {Array.new(20)}
+  /fill the array with random capital letters/
+  wordSearchArray.each_index do |row|
+    wordSearchArray[row].each_index do |col|
+	  wordSearchArray[row][col] = (65 + rand(26)).chr
 	end
   end
+
   /keep track of which lines we have already put a word on/
   linesUsed = Array.new()
 
   inputWords.each do |word|
     word = word.upcase
     vertPos = rand(20)
-	/Make sure we haven't put a word on this line yet/
+
+	/make sure we haven't put a word on this line yet/
     while linesUsed.include?(vertPos)
 	  vertPos = rand(20)
 	end
 	/choose a starting point in the row/
-	horizRand = rand(20 - word.length-1)
+	horizRand = rand(21 - word.length)
+
 	/place characters into the wordSearchArray/
     for place in (0..(word.length-1))
 	  horizPos = horizRand + place
       wordSearchArray[vertPos][horizPos] = word[place]
 	end
   end
+
   /output the wordSearchArray filled with the user's words/
   wordSearchArray.each do |arg|
     puts arg.each{|line| line}.join(" ")
