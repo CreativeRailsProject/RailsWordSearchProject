ea959139e93d728859cb183d690d93b76b9cd905
cvalleroy <cvalleroy@hmc.edu>
2018-02-09 12:57:13 -0800

    Comment fixes and file removal

--- WordSearch/app/controllers/searches_controller.rb
+++ WordSearch/app/controllers/searches_controller.rb
@@ -14,9 +14,7 @@ class SearchesController < ApplicationController
 	@search = Search.new
 
 	# Get words from input and place in array
-	words = params[:search][:words][0].upcase
 	wordsArr = words.split(/\s*,\s*/)
-	puts wordsArr
 
 	# Place words into search object
 	@search.words = wordsArr
@@ -24,31 +22,31 @@ class SearchesController < ApplicationController
 	# Generate search based on wordsArr
 	# and place into @search.word_search. (@search.word_search = 20x20arr)
 	
-	/create the blank 20x20 array/
+	# Create the blank 20x20 array
 	wordSearchArray = Array.new(20) {Array.new(20, "-")}
 	wordSearchKey = Array.new(20) {Array.new(20, "-")}
-	/fill the array with random capital letters/
+	# Fill the array with random capital letters
 	wordSearchArray.each_index do |row|
 		wordSearchArray[row].each_index do |col|
 			wordSearchArray[row][col] = (65 + rand(26)).chr
 		end
 	end
 
-	/keep track of which lines we have already put a word on/
+	# Keep track of which lines we have already put a word on
 	linesUsed = Array.new()
 
 	wordsArr.each do |word|
 		word = word.upcase
 		vertPos = rand(20)
 
-		/make sure we haven't put a word on this line yet/
+		# Make sure we haven't put a word on this line yet
 		while linesUsed.include?(vertPos)
 			vertPos = rand(20)
 		end
-		/choose a starting point in the row/
+		# Choose a starting point in the row
 		horizRand = rand(21 - word.length)
 
-		/place characters into the wordSearchArray/
+		# Place characters into the wordSearchArray
 		for place in (0..(word.length-1))
 			horizPos = horizRand + place
 			wordSearchArray[vertPos][horizPos] = word[place]
@@ -58,7 +56,7 @@ class SearchesController < ApplicationController
 	@search.key = wordSearchKey
 	@search.word_search = wordSearchArray
 	
-	/output the wordSearchArray filled with the user's words/
+	# Output the wordSearchArray filled with the user's words
 	puts "Word Search"
 	wordSearchArray.each do |arg|
 		puts arg.each{|line| line}.join(" ")
