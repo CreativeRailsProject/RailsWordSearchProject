4be11e5a8c60272587eb2185ec4c0d021549b805
jlkunzelman <jkunzelman@g.hmc.edu>
2018-02-08 22:47:26 -0800

    Deleted unnecessary TODO

--- WordSearch/app/controllers/application_controller.rb
+++ WordSearch/app/controllers/application_controller.rb
@@ -1,8 +1,4 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
-  # TODO: Replace test fucntion with Code to Generate word search in here
-  def test
-	puts "hi"
-  end
 end
