cfff33ecd5db06d646e70e609d118f735e334da7
cvalleroy <cvalleroy@hmc.edu>
2018-02-10 14:08:15 -0800

    Links between different pages working, Key page created

--- WordSearch/app/controllers/searches_controller.rb
+++ WordSearch/app/controllers/searches_controller.rb
@@ -24,7 +24,7 @@ class SearchesController < ApplicationController
 	# and place into @search.word_search. (@search.word_search = 20x20arr)
 	
 	# Create the blank 20x20 array
-	wordSearchArray = Array.new(20) {Array.new(20, "-")}
+	wordSearchArray = Array.new(20) {Array.new(20)}
 	wordSearchKey = Array.new(20) {Array.new(20, "-")}
 	# Fill the array with random capital letters
 	wordSearchArray.each_index do |row|
@@ -44,6 +44,7 @@ class SearchesController < ApplicationController
 		while linesUsed.include?(vertPos)
 			vertPos = rand(20)
 		end
+		linesUsed.push(vertPos)
 		# Choose a starting point in the row
 		horizRand = rand(21 - word.length)
 		# Randomly decide if the word should be flipped when put into the word search
@@ -88,6 +89,11 @@ class SearchesController < ApplicationController
 
   def show
 	# Get the search object from database based on the id
+	@search = Search.find(params[:id])
+  end
+
+  def key
+	# Get the key object from database based on the id
 	@search = Search.find(params[:id])
   end
 
